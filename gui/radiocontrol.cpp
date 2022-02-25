2f5d32e42d354800abbfba436ab148e794852ed6
Petr Kopecky <xkejpi@gmail.com>
2022-02-25 19:56:10 +0100

    Searching for programme service fixed, SPI disabled, causes some error - to be analyzed

--- gui/radiocontrol.cpp
+++ gui/radiocontrol.cpp
@@ -377,9 +377,9 @@ void RadioControl::eventFromDab(RadioControlEvent * pEvent)
 //#warning "Remove automatic Journaline - this is for debug only"
 //                        startUserApplication(DabUserApplicationType::Journaline, true);
 #warning "Remove automatic SPI - this is for debug only"
-                        startUserApplication(DabUserApplicationType::SPI, true);
+                        //startUserApplication(DabUserApplicationType::SPI, true);
 #warning "Remove automatic TPEG - this is for debug only"
-                        startUserApplication(DabUserApplicationType::TPEG, true);
+                        //startUserApplication(DabUserApplicationType::TPEG, true);
                     }
                 }
             }
