6cf2c713bd30d846cbe4c1d836822e50d66aa3bd
Petr Kopecky <xkejpi@gmail.com>
2022-03-06 20:51:10 +0100

    Version exported from cmake

--- gui/aboutdialog.cpp
+++ gui/aboutdialog.cpp
@@ -1,5 +1,7 @@
+#include <QDesktopServices>
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
+#include "config.h"
 
 AboutDialog::AboutDialog(QWidget *parent) :
     QDialog(parent),
@@ -8,17 +10,23 @@ AboutDialog::AboutDialog(QWidget *parent) :
     ui->setupUi(this);
 
     ui->appName->setText("<b>Abraca DAB radio</b>");
-    //ui->author->setText("Developed by <a href=\"mailto:xkejpi@gmail.com\">Petr Kopecky</a>");
     ui->author->setText("Developed by Petr Kopecky (<a href=\"mailto:xkejpi@gmail.com\">xkejpi@gmail.com</a>)");
-    ui->libraries->setText("AbracaDABra links following libraries (special thanks to):"
+#ifdef PROJECT_VERSION_RELEASE
+    ui->version->setText(QString("Version %1, revision %2")
+#else
+    ui->version->setText(QString("Version %1+, revision %2")
+#endif
+                         .arg(PROJECT_VER).arg(PROJECT_GIT_REV));
+    ui->qtVersion->setText(QString("Based on QT %1").arg(QT_VERSION_STR));
+    ui->libraries->setText("AbracaDABra uses following libraries (special thanks to):"
                            "<ul>"
                            "<li><a href=\"https://github.com/anthonix/ffts\">FFTS</a> by Anthony Blake</li>"
                            "<li><a href=\"https://github.com/anthonix/ffts\">KISS FFT</a> by Mark Borgerding</li>"
                            "<li><a href=\"https://github.com/Opendigitalradio/ka9q-fec\">FEC</a> by Phil Karn, KA9Q</li>"
                            "<li><a href=\"https://osmocom.org/projects/rtl-sdr/wiki/Rtl-sdr\">rtl-sdr</a> by Steve Markgraf, Dimitri Stolnikov, and Hoernchen, with contributions by Kyle Keen, Christian Vogel and Harald Welte.</li>"
                            "<li><a href=\"https://www.mpg123.dec\">mpg123</a> by Michael Hipp, Thomas Orgis and others</li>"
-                           "<li><a href=\"https://github.com/knik0/faad2\">FAAD2</a> Copyright (C) 2003-2005 M. Bakker, Nero AG</li>"
-                           "<li><a href=\"http://www.portaudio.com\">PortAudio</a> Copyright (c) 1999-2011 Ross Bencina and Phil Burk</li>"
+                           "<li><a href=\"https://github.com/knik0/faad2\">FAAD2</a> Copyright © 2003-2005 M. Bakker, Nero AG</li>"
+                           "<li><a href=\"http://www.portaudio.com\">PortAudio</a> Copyright © 1999-2011 Ross Bencina and Phil Burk</li>"
                            "</ul>");
     ui->disclaimer->setText("<p>Copyright © 2019-2022 Petr Kopecký</p>"
                             "<p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software "
@@ -33,6 +41,15 @@ AboutDialog::AboutDialog(QWidget *parent) :
                             "IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, "
                             "WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH "
                             "THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.");
+
+    QObject::connect(
+                ui->author, &QLabel::linkActivated,
+                [=]( const QString & link ) { QDesktopServices::openUrl(QUrl::fromUserInput(link)); }
+            );
+    QObject::connect(
+                ui->libraries, &QLabel::linkActivated,
+                [=]( const QString & link ) { QDesktopServices::openUrl(QUrl::fromUserInput(link)); }
+            );
 }
 
 AboutDialog::~AboutDialog()
