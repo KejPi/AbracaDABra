424c606128ed8e4295dfbe2d831ab83675073950
KejPi <xkejpi@gmail.com>
2022-06-24 21:37:00 +0200

    Icons reorganized, icon for linux added, preparation for AppImage creating

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include "config.h"
 
 int main(int argc, char *argv[])
 {
+
+    QCoreApplication::setApplicationName("AbracaDABra");
+    QCoreApplication::setApplicationVersion(PROJECT_VER);
+
     QApplication a(argc, argv);
+
+#ifdef Q_OS_LINUX
+    // Set icon
+    a.setWindowIcon(QIcon(":/resources/appIcon-linux.png"));
+#endif
+
     MainWindow w;
-    w.show();
+    w.show();        
     return a.exec();
 }
