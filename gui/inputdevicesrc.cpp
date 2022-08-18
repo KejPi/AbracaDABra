0c421e86878ab6a3774fda12a83d90c33a35bc22
KejPi <xkejpi@gmail.com>
2022-08-18 14:20:23 +0200

    Transposed Farrow implemented - not tested yet

--- gui/inputdevicesrc.cpp
+++ gui/inputdevicesrc.cpp
@@ -1,4 +1,7 @@
+#include <QDebug>
 #include "inputdevicesrc.h"
+#include <cmath>
+#include <cstring>
 
 InputDeviceSRC::InputDeviceSRC(int inputSampleRate)
 {
@@ -6,10 +9,11 @@ InputDeviceSRC::InputDeviceSRC(int inputSampleRate)
     if (2*2048000 == inputSampleRate)
     {
         m_filter = new InputDeviceSRCFilterDS2();
+        //m_filter = new InputDeviceSRCFilterFarrow(inputSampleRate);
     }
     else
-    {   // not supported currently
-        m_filter = new InputDeviceSRCFilterDS2();
+    {
+        m_filter = new InputDeviceSRCFilterFarrow(inputSampleRate);
     }
 }
 
@@ -48,6 +52,9 @@ InputDeviceSRCFilterDS2::InputDeviceSRCFilterDS2()
     bufferPtrI = bufferI;
     bufferPtrQ = bufferQ;
 
+    catt = 1 - std::exp(-1/(INPUTDEVICESRC_LEVEL_ATTACK * 2048e3));
+    crel = 1 - std::exp(-1/(INPUTDEVICESRC_LEVEL_RELEASE * 2048e3));
+
     InputDeviceSRCFilterDS2::reset();
 }
 
@@ -72,9 +79,6 @@ void InputDeviceSRCFilterDS2::reset()
 
 int InputDeviceSRCFilterDS2::process(float inDataIQ[], int numInDataIQ, float outDataIQ[])
 {
-#define LEV_CATT 0.01
-#define LEV_CREL 0.00001
-
     float level = m_signalLevel;
 
     float * bufPtrI = bufferPtrI;
@@ -119,7 +123,6 @@ int InputDeviceSRCFilterDS2::process(float inDataIQ[], int numInDataIQ, float ou
             bufPtrQ = bufferQ;
         }
 
-        // insert new samples to delay line
 #if (INPUTDEVICESRC_LEVEL_ESTIMATION > 0)
         float abs2 = (*inDataIQ) * (*inDataIQ);  // I*I
 
@@ -134,10 +137,10 @@ int InputDeviceSRCFilterDS2::process(float inDataIQ[], int numInDataIQ, float ou
         *bufPtrQ++ = *inDataIQ++;                // Q
 
         // calculate signal level (rectifier, fast attack slow release)
-        float c = LEV_CREL;
+        float c = crel;
         if (abs2 > level)
         {
-            c = LEV_CATT;
+            c = catt;
         }
         level = c * abs2 + level - c * level;
 #else
@@ -161,4 +164,118 @@ int InputDeviceSRCFilterDS2::process(float inDataIQ[], int numInDataIQ, float ou
     m_signalLevel = level;
 
     return numInDataIQ/2;
+}
+
+//===================================================================================================
+// Transposed Farrow filter designed for downsampling from arbitrary rate to 2048kHz
+
+InputDeviceSRCFilterFarrow::InputDeviceSRCFilterFarrow(int inputSampleRate)
+{
+    // output FS is fixed to 2048kHz
+    R = 2048e3 / (float) inputSampleRate;
+
+    // calculate catt and crel
+    catt = 1 - std::exp(-1/(INPUTDEVICESRC_LEVEL_ATTACK * inputSampleRate));
+    crel = 1 - std::exp(-1/(INPUTDEVICESRC_LEVEL_RELEASE * inputSampleRate));
+}
+
+InputDeviceSRCFilterFarrow::~InputDeviceSRCFilterFarrow()
+{
+
+}
+
+void InputDeviceSRCFilterFarrow::reset()
+{
+    mu = 0;
+    for (int m = 0; m < POLY_COEFS; ++m)
+    {
+        xI[m] = 0.0;
+        xQ[m] = 0.0;
+    }
+
+    for (int n = 0; n < NUM_POLY; ++n)
+    {
+        yI[n] = 0.0;
+        yQ[n] = 0.0;
+    }
+}
+
+int InputDeviceSRCFilterFarrow::process(float inDataIQ[], int numInDataIQ, float outDataIQ[])
+{
+    float level = m_signalLevel;
+    int numOutDataIQ = 0;
+
+    // do for all input samples
+    do
+    {
+        mu = mu - R;
+        if (mu < 0)
+        {   // dump condition
+            mu = mu + 1.0;
+
+            // calc FIR for each polynomial
+            for (int n = 0; n<NUM_POLY; ++n)
+            {
+                float accI = 0;
+                float accQ = 0;
+                for (int m = 0; m < POLY_COEFS; ++m)
+                {
+                    accI = accI + xI[m] * coef[n][m];
+                    accQ = accQ + xQ[m] * coef[n][m];
+                }
+                yI[n] = yI[n] + accI;
+                yQ[n] = yQ[n] + accQ;
+            }
+            *outDataIQ++ = yI[0];
+            *outDataIQ++ = yQ[0];
+            numOutDataIQ += 1;
+
+            // shift delay line -> prepare for next dump
+            std::memmove(&yI[0], &yI[1], (NUM_POLY-1)*sizeof(float));
+            std::memmove(&yQ[0], &yQ[1], (NUM_POLY-1)*sizeof(float));
+            yI[NUM_POLY-1] = 0.0;
+            yQ[NUM_POLY-1] = 0.0;
+
+            // dump xIQ
+            for (int m = 0; m < POLY_COEFS; ++m)
+            {
+                xI[m] = 0.0;
+                xQ[m] = 0.0;
+            }
+        }
+        else { /* continue with integration */ }
+
+        float inI = *inDataIQ++;
+        float inQ = *inDataIQ++;
+
+#if (INPUTDEVICESRC_LEVEL_ESTIMATION > 0)
+        float abs2 = inI * inI + inQ * inQ;
+
+        // calculate signal level (rectifier, fast attack slow release)
+        float c = crel;
+        if (abs2 > level)
+        {
+            c = catt;
+        }
+        level = c * abs2 + level - c * level;
+#endif
+
+        // Integrate
+        xI[0] += inI;
+        xQ[0] += inQ;
+
+        for (int m = 1; m < POLY_COEFS; ++m)
+        {
+            inI = inI*mu;
+            inQ = inQ*mu;
+
+            xI[m] += inI;
+            xQ[m] += inQ;
+        }
+    } while (--numInDataIQ > 0);
+
+    // store signal level
+    m_signalLevel = level;
+
+    return numOutDataIQ;
 }
