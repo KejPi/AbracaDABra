e409c054bd604fec92047e3cbf53b1c1bb2c41a4
KejPi <xkejpi@gmail.com>
2023-01-16 09:23:49 +0100

    DABSDR 2.0.0 Windows build updated

---
+++ lib/windows_amd64/cmake/dabsdr-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "dabsdr::dabsdr" for configuration "Release"
+set_property(TARGET dabsdr::dabsdr APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(dabsdr::dabsdr PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/windows_amd64/libdabsdr.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/windows_amd64/libdabsdr.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS dabsdr::dabsdr )
+list(APPEND _IMPORT_CHECK_FILES_FOR_dabsdr::dabsdr "${_IMPORT_PREFIX}/windows_amd64/libdabsdr.dll.a" "${_IMPORT_PREFIX}/windows_amd64/libdabsdr.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
