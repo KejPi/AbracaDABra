2e4689503d85ccfa29fe0af5f1d4aceb2cd806da
KejPi <xkejpi@gmail.com>
2023-08-23 21:42:52 +0200

    DABSDR 2.3.3 Linux AARCH64 build: compatibility with obsolete DAB standard, forcing SCIdS to 0 for primary services

--- lib/linux_aarch64/cmake/dabsdr-release.cmake
+++ lib/linux_aarch64/cmake/dabsdr-release.cmake
@@ -8,12 +8,12 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "dabsdr::dabsdr" for configuration "Release"
 set_property(TARGET dabsdr::dabsdr APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(dabsdr::dabsdr PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/linux_aarch64/libdabsdr.so.2.3.2"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/linux_aarch64/libdabsdr.so.2.3.3"
   IMPORTED_SONAME_RELEASE "libdabsdr.so.2"
   )
 
 list(APPEND _cmake_import_check_targets dabsdr::dabsdr )
-list(APPEND _cmake_import_check_files_for_dabsdr::dabsdr "${_IMPORT_PREFIX}/linux_aarch64/libdabsdr.so.2.3.2" )
+list(APPEND _cmake_import_check_files_for_dabsdr::dabsdr "${_IMPORT_PREFIX}/linux_aarch64/libdabsdr.so.2.3.3" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
