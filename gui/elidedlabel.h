0a71e7911b9b399d177caeee4acab3f9eb7f6877
Petr Kopecky <xkejpi@gmail.com>
2024-01-08 19:46:40 +0100

    Elided label event loop fixed

--- gui/elidedlabel.h
+++ gui/elidedlabel.h
@@ -1,36 +1,55 @@
-// this sourcecode is from: https://wiki.qt.io/Elided_Label
+/*
+ * This file is part of the AbracaDABra project
+ *
+ * MIT License
+ *
+  * Copyright (c) 2019-2024 Petr Kopecký <xkejpi (at) gmail (dot) com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #ifndef ELIDEDLABEL_H
 #define ELIDEDLABEL_H
 
 #include <QLabel>
-
-#include <QLabel>
-
-// MIT - Yash : Speedovation.com [ Picked from internet and modified. if owner needs to add or change license do let me know.]
+#include <QPainter>
 
 class ElidedLabel : public QLabel
 {
     Q_OBJECT
-public:
-    using QLabel::QLabel;
 
-    // Set the elide mode used for displaying text.
+public:
+    explicit ElidedLabel(QWidget *parent = nullptr);
+
+    void setText(const QString &text);
+    const QString & text() const { return content; }
+
     void setElideMode(Qt::TextElideMode elideMode);
-    // Get the elide mode currently used to display text.
     Qt::TextElideMode elideMode() const { return m_elideMode; }
 
 protected:
-    void paintEvent(QPaintEvent *e) override;
-    void resizeEvent(QResizeEvent *e) override;
-
-private:
-    void updateCachedTexts();
+    void paintEvent(QPaintEvent *event) override;
 
 private:
     Qt::TextElideMode m_elideMode = Qt::ElideRight;
-    QString m_cachedElidedText;
-    QString m_cachedText;
+    QString content;
 };
+
 
 #endif // ELIDEDLABEL_H
