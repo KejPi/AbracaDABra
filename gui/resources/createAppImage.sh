fac94cd231bd519664c2337ebf67f356d76198fd
KejPi <xkejpi@gmail.com>
2024-05-06 20:59:37 +0200

    fix: Mouse wheel supprt in TII map, script for building appImage updated

--- gui/resources/createAppImage.sh
+++ gui/resources/createAppImage.sh
@@ -6,6 +6,12 @@ RESOURCES_DIR=$PWD/gui/resources
 QML_DIR=$PWD/gui/qml
 VER=`git describe`
 
+if [ -z ${QT_PATH+x} ]; then
+	QMAKE=$(which qmake6)
+else
+	QMAKE=$QT_PATH/bin/qmake
+fi
+
 echo "Building application version: $VER"
 
 # Build 
@@ -15,7 +21,13 @@ fi
 mkdir build
 cd build
 #cmake .. -DAIRSPY=ON -DSOAPYSDR=ON -DPROJECT_VERSION_RELEASE=ON -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib
-cmake .. -DAIRSPY=ON -DSOAPYSDR=ON -DPROJECT_VERSION_RELEASE=ON -DUSE_SYSTEM_RTLSDR=OFF -DCMAKE_INSTALL_PREFIX=/usr
+
+if [ -z ${QT_PATH+x} ]; then
+	cmake .. -DAIRSPY=ON -DSOAPYSDR=ON -DPROJECT_VERSION_RELEASE=ON -DUSE_SYSTEM_RTLSDR=OFF -DUSE_SYSTEM_QCUSTOMPLOT=OFF -DCMAKE_INSTALL_PREFIX=/usr
+else 
+	cmake .. -DAIRSPY=ON -DSOAPYSDR=ON -DPROJECT_VERSION_RELEASE=ON -DUSE_SYSTEM_RTLSDR=OFF -DUSE_SYSTEM_QCUSTOMPLOT=OFF -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_PREFIX_PATH=$QT_PATH/lib/cmake
+fi
+
 make -j$(nproc)
 make install DESTDIR=$APPDIR
 
@@ -23,7 +35,12 @@ make install DESTDIR=$APPDIR
 rm -Rf $APPDIR/usr/lib/*
 cp -f gui/AbracaDABra $APPDIR/usr/bin/
 
-
-QML_SOURCES_PATHS=$QML_DIR QMAKE=$(which qmake6) VERSION=$VER linuxdeploy --appdir $APPDIR -d ${RESOURCES_DIR}/AbracaDABra.desktop -i $ICON_DIR/AbracaDABra.png --plugin qt --output appimage
+if [ -z ${QT_PATH+x} ]; then
+	QML_SOURCES_PATHS=$QML_DIR QMAKE=$(which qmake6) VERSION=$VER EXTRA_QT_PLUGINS=location \
+		linuxdeploy --appdir $APPDIR -d ${RESOURCES_DIR}/AbracaDABra.desktop -i $ICON_DIR/AbracaDABra.png --plugin qt --output appimage	
+else
+	QML_SOURCES_PATHS=$QML_DIR QMAKE=$QT_PATH/bin/qmake VERSION=$VER EXTRA_QT_PLUGINS=location \
+		linuxdeploy --appdir $APPDIR -d ${RESOURCES_DIR}/AbracaDABra.desktop -i $ICON_DIR/AbracaDABra.png --plugin qt --output appimage
+fi
 
 
