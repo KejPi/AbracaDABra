70f0fb641ea12f4866d30e8a26e2a0b2a8292684
KejPi <xkejpi@gmail.com>
2024-06-06 21:20:33 +0200

    DABSDR version 2.5.93 (Linux x86_64 build)

--- lib/linux_x86_64/cmake/dabsdr-release.cmake
+++ lib/linux_x86_64/cmake/dabsdr-release.cmake
@@ -8,12 +8,12 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "dabsdr::dabsdr" for configuration "Release"
 set_property(TARGET dabsdr::dabsdr APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(dabsdr::dabsdr PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/linux_x86_64/libdabsdr.so.2.5.92"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/linux_x86_64/libdabsdr.so.2.5.93"
   IMPORTED_SONAME_RELEASE "libdabsdr.so.2"
   )
 
 list(APPEND _IMPORT_CHECK_TARGETS dabsdr::dabsdr )
-list(APPEND _IMPORT_CHECK_FILES_FOR_dabsdr::dabsdr "${_IMPORT_PREFIX}/linux_x86_64/libdabsdr.so.2.5.92" )
+list(APPEND _IMPORT_CHECK_FILES_FOR_dabsdr::dabsdr "${_IMPORT_PREFIX}/linux_x86_64/libdabsdr.so.2.5.93" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
