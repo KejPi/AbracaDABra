d48c49f143801e23534f1bd6b996381ebaca0d6d
KejPi <xkejpi@gmail.com>
2024-09-11 21:39:45 +0200

    DABSDR 2.5.95 and FMLIST interface libs for Linux x86_64

---
+++ lib/linux_x86_64/cmake/fmlistInterface-release.cmake
@@ -0,0 +1,20 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "fmlistInterface::fmlistInterface" for configuration "Release"
+set_property(TARGET fmlistInterface::fmlistInterface APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fmlistInterface::fmlistInterface PROPERTIES
+  IMPORTED_LINK_DEPENDENT_LIBRARIES_RELEASE "Qt6::Core;Qt6::Gui;Qt6::Network"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/linux_x86_64/libfmlistInterface.so.1.0.0"
+  IMPORTED_SONAME_RELEASE "libfmlistInterface.so.1"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fmlistInterface::fmlistInterface )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fmlistInterface::fmlistInterface "${_IMPORT_PREFIX}/linux_x86_64/libfmlistInterface.so.1.0.0" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
