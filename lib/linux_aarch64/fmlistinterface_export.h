b4e746d12d559a3e6dee315155d47fd95e5f4bf0
KejPi <xkejpi@gmail.com>
2024-09-11 20:34:52 +0200

    DABSDR 2.5.95 and FMLIST interface libs for Linux AARCH64

---
+++ lib/linux_aarch64/fmlistinterface_export.h
@@ -0,0 +1,43 @@
+
+#ifndef FMLISTINTERFACE_API_H
+#define FMLISTINTERFACE_API_H
+
+#ifdef FMLISTINTERFACE_STATIC_DEFINE
+#  define FMLISTINTERFACE_API
+#  define FMLISTINTERFACE_NO_EXPORT
+#else
+#  ifndef FMLISTINTERFACE_API
+#    ifdef fmlistInterface_EXPORTS
+        /* We are building this library */
+#      define FMLISTINTERFACE_API __attribute__((visibility("default")))
+#    else
+        /* We are using this library */
+#      define FMLISTINTERFACE_API __attribute__((visibility("default")))
+#    endif
+#  endif
+
+#  ifndef FMLISTINTERFACE_NO_EXPORT
+#    define FMLISTINTERFACE_NO_EXPORT __attribute__((visibility("hidden")))
+#  endif
+#endif
+
+#ifndef FMLISTINTERFACE_DEPRECATED
+#  define FMLISTINTERFACE_DEPRECATED __attribute__ ((__deprecated__))
+#endif
+
+#ifndef FMLISTINTERFACE_DEPRECATED_EXPORT
+#  define FMLISTINTERFACE_DEPRECATED_EXPORT FMLISTINTERFACE_API FMLISTINTERFACE_DEPRECATED
+#endif
+
+#ifndef FMLISTINTERFACE_DEPRECATED_NO_EXPORT
+#  define FMLISTINTERFACE_DEPRECATED_NO_EXPORT FMLISTINTERFACE_NO_EXPORT FMLISTINTERFACE_DEPRECATED
+#endif
+
+/* NOLINTNEXTLINE(readability-avoid-unconditional-preprocessor-if) */
+#if 0 /* DEFINE_NO_DEPRECATED */
+#  ifndef FMLISTINTERFACE_NO_DEPRECATED
+#    define FMLISTINTERFACE_NO_DEPRECATED
+#  endif
+#endif
+
+#endif /* FMLISTINTERFACE_API_H */
