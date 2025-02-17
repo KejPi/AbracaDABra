5380c28ecc6a2596de6821247ee8c4197ee3032f
KejPi <xkejpi@gmail.com>
2025-02-17 19:16:06 +0100

    fix: App version

--- gui/appversion.h
+++ gui/appversion.h
@@ -37,33 +37,33 @@ public:
     AppVersion() {}
     AppVersion(const QString& verString)
     {
-        static const QRegularExpression verRe("v(\\d+)\\.(\\d+)\\.(\\d+)(-(\\d+)-)?");
+        static const QRegularExpression verRe("v(\\d+)\\.(\\d+)\\.(\\d+)(-(\\d+))?");
         QRegularExpressionMatch verMatch = verRe.match(verString);
         if (verMatch.hasMatch())
         {
-            m_major = verMatch.captured(1).toInt();
-            m_minor = verMatch.captured(2).toInt();
-            m_patch = verMatch.captured(3).toInt();
+            m_major = verMatch.captured(1).toUInt();
+            m_minor = verMatch.captured(2).toUInt();
+            m_patch = verMatch.captured(3).toUInt();
             if (!verMatch.captured(5).isNull())
             {
-                m_git = verMatch.captured(5).toInt();
+                m_git = verMatch.captured(5).toUInt();
             }
             // qDebug() << m_major << m_minor << m_patch << m_git;
         }
     }
-    bool operator==(const AppVersion& other) const { return toInt64() == other.toInt64(); }
-    bool operator>(const AppVersion& other) const { return toInt64() > other.toInt64(); }
-    bool operator>=(const AppVersion& other) const { return toInt64() >= other.toInt64(); }
+    bool operator==(const AppVersion& other) const { return toUInt64() == other.toUInt64(); }
+    bool operator>(const AppVersion& other) const { return toUInt64() > other.toUInt64(); }
+    bool operator>=(const AppVersion& other) const { return toUInt64() >= other.toUInt64(); }
     bool operator<(const AppVersion& other) const { return !(*this >= other); }
     bool operator<=(const AppVersion& other) const { return !(*this > other); }
     bool isValid() const { return m_major != 0; }
 
 private:
-    int m_major = 0;
-    int m_minor = 0;
-    int m_patch = 0;
-    int m_git = 0;
-    int64_t toInt64() const { return int64_t((m_major << 30) + (m_minor << 20) + (m_patch << 10) + m_git); }
+    uint m_major = 0;
+    uint m_minor = 0;
+    uint m_patch = 0;
+    uint m_git = 0;
+    uint64_t toUInt64() const { return uint64_t((m_major << 30) + (m_minor << 20) + (m_patch << 10) + m_git); }
 };
 
 #endif  // APPVERSION_H
