1ebe9457b1ad472c56e1287aa1a488139b51bd9d
KejPi <xkejpi@gmail.com>
2025-03-07 19:54:04 +0100

    DABSDR 3.0.0 and FMLIST interface library 2.0.0 for Linux x86_64

--- lib/linux_x86_64/cmake/fmlistInterface-release.cmake
+++ lib/linux_x86_64/cmake/fmlistInterface-release.cmake
@@ -9,12 +9,12 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 set_property(TARGET fmlistInterface::fmlistInterface APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(fmlistInterface::fmlistInterface PROPERTIES
   IMPORTED_LINK_DEPENDENT_LIBRARIES_RELEASE "Qt6::Core;Qt6::Gui;Qt6::Network"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/linux_x86_64/libfmlistInterface.so.1.0.1"
-  IMPORTED_SONAME_RELEASE "libfmlistInterface.so.1"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/linux_x86_64/libfmlistInterface.so.2.0.0"
+  IMPORTED_SONAME_RELEASE "libfmlistInterface.so.2"
   )
 
 list(APPEND _IMPORT_CHECK_TARGETS fmlistInterface::fmlistInterface )
-list(APPEND _IMPORT_CHECK_FILES_FOR_fmlistInterface::fmlistInterface "${_IMPORT_PREFIX}/linux_x86_64/libfmlistInterface.so.1.0.1" )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fmlistInterface::fmlistInterface "${_IMPORT_PREFIX}/linux_x86_64/libfmlistInterface.so.2.0.0" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
