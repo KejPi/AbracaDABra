8f7829b3eae3d96bf5a2822f62b263c89e8a55e6
KejPi <xkejpi@gmail.com>
2025-04-21 19:32:45 +0200

    DABSDR 3.1.0 for macOS: audio service bitrate stats

--- lib/darwin_x86_64/cmake/dabsdr-release.cmake
+++ lib/darwin_x86_64/cmake/dabsdr-release.cmake
@@ -8,12 +8,12 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "dabsdr::dabsdr" for configuration "Release"
 set_property(TARGET dabsdr::dabsdr APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(dabsdr::dabsdr PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/darwin_x86_64/libdabsdr.3.0.0.dylib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/darwin_x86_64/libdabsdr.3.1.0.dylib"
   IMPORTED_SONAME_RELEASE "@rpath/libdabsdr.3.dylib"
   )
 
 list(APPEND _cmake_import_check_targets dabsdr::dabsdr )
-list(APPEND _cmake_import_check_files_for_dabsdr::dabsdr "${_IMPORT_PREFIX}/darwin_x86_64/libdabsdr.3.0.0.dylib" )
+list(APPEND _cmake_import_check_files_for_dabsdr::dabsdr "${_IMPORT_PREFIX}/darwin_x86_64/libdabsdr.3.1.0.dylib" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
